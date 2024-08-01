132df06077a6cdc898e9df528cc630115309e78c
Farhad F <table.picture@hotmail.com>
2024-08-01 21:24:15 +0330

    Add project files.

---
+++ BlazorApp3/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp3.Models
+{
+    public class Person()
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
