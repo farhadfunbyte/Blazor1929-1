b320096b92a4de47a4b5d639b7e7bd5cc7541e97
Farhad F <table.picture@hotmail.com>
2024-09-23 20:04:03 +0330

    js

--- BlazorApp3/Program.cs
+++ BlazorApp3/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp3;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSweetAlert2(options => {
+    options.Theme = SweetAlertTheme.MaterialUI;
+});
 
 await builder.Build().RunAsync();
---
+++ BlazorApp3/wwwroot/js/test_script.js
@@ -0,0 +1,13 @@
+﻿function sweet_alert(title, text, icon) {
+    
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: icon
+    });
+     
+}
+
+function test_confirm(text) {
+    return confirm(text);
+}
